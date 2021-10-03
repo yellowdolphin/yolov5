8409925a22f24175e8ae30c91a6cea88fecc0e50
yellowdolphin <marius.wanko@gmail.com>
2021-10-04 01:31:08 +0200

    add aux_loss

--- utils/loss.py
+++ utils/loss.py
@@ -105,7 +105,22 @@ class ComputeLoss:
         if g > 0:
             BCEcls, BCEobj = FocalLoss(BCEcls, g), FocalLoss(BCEobj, g)
 
-        det = model.module.model[-1] if is_parallel(model) else model.model[-1]  # Detect() module
+        #det = model.module.model[-1] if is_parallel(model) else model.model[-1]  # Detect() module
+        # For V5Centernet
+        if hasattr(model, 'module'): #multi-gpu training
+            if hasattr(model.module, 'model'):
+                det = model.module.model[-1]
+            else:
+                det = model.module.model.detection
+        else:
+            if hasattr(model, 'model'):
+                try:
+                    det = model.model.detection
+                except:
+                    det = model.model.model[-1] 
+            else:
+                det = model.detection
+
         self.balance = {3: [4.0, 1.0, 0.4]}.get(det.nl, [4.0, 1.0, 0.25, 0.06, .02])  # P3-P7
         self.ssi = list(det.stride).index(16) if autobalance else 0  # stride 16 index
         self.BCEcls, self.BCEobj, self.gr, self.hyp, self.autobalance = BCEcls, BCEobj, 1.0, h, autobalance
