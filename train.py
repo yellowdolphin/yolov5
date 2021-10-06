c61bf696291fb079f7ea0b44af3e78265a793d66
yellowdolphin <marius.wanko@gmail.com>
2021-10-06 15:16:13 +0200

    switch to focal_loss

--- train.py
+++ train.py
@@ -90,30 +90,30 @@ class FocalLoss(nn.Module):
             pred (batch x c x h x w)
             gt_regr (batch x c x h x w)
         '''
-      # gt[torch.where(gt>0.99)]=1
+        # gt[torch.where(gt>0.99)]=1
 
-      # pos_inds = gt.eq(1).float()
-      # neg_inds = gt.lt(1).float()
+        # pos_inds = gt.eq(1).float()
+        # neg_inds = gt.lt(1).float()
 
-      pos_inds = gt.lt(0).float()
-      neg_inds = gt.eq(0).float()
+        pos_inds = gt.lt(0).float()
+        neg_inds = gt.eq(0).float()
 
-      neg_weights = torch.pow(1 - gt, 4)
+        neg_weights = torch.pow(1 - gt, 4)
 
-      loss = 0
+        loss = 0
 
-      pos_loss = torch.log(pred) * torch.pow(1 - pred, 2) * pos_inds
-      neg_loss = torch.log(1 - pred) * torch.pow(pred, 2) * neg_weights * neg_inds
+        pos_loss = torch.log(pred) * torch.pow(1 - pred, 2) * pos_inds
+        neg_loss = torch.log(1 - pred) * torch.pow(pred, 2) * neg_weights * neg_inds
 
-      num_pos  = pos_inds.float().sum()
-      pos_loss = pos_loss.sum()
-      neg_loss = neg_loss.sum()
+        num_pos  = pos_inds.float().sum()
+        pos_loss = pos_loss.sum()
+        neg_loss = neg_loss.sum()
 
-      if num_pos == 0:
-          loss = loss - neg_loss
-      else:
-          loss = loss - (pos_loss + neg_loss) / num_pos
-      return loss
+        if num_pos == 0:
+            loss = loss - neg_loss
+        else:
+            loss = loss - (pos_loss + neg_loss) / num_pos
+        return loss
 
 
 
