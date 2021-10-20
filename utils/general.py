43ecf1010cbeacc08e8798e677a276372b008154
yellowdolphin <marius.wanko@gmail.com>
2021-10-20 16:43:27 +0200

    merge debug

--- utils/general.py
+++ utils/general.py
@@ -587,7 +587,8 @@ def non_max_suppression(prediction, conf_thres=0.25, iou_thres=0.45, classes=Non
         i = torchvision.ops.nms(boxes, scores, iou_thres)  # NMS
         if i.shape[0] > max_det:  # limit detections
             i = i[:max_det]
-        if merge #and (1 < n < 3E3):  # Merge NMS (boxes merged using weighted mean)
+        #if merge and (1 < n < 3E3):  # Merge NMS (boxes merged using weighted mean)
+        if merge:
             # update boxes as boxes(i,4) = weights(i,n) * boxes(n,4)
             iou = box_iou(boxes[i], boxes) > iou_thres  # iou matrix
             weights = iou * scores[None]  # box weights
