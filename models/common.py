128dd4ba10c0b1cf2daa0731f72c7fdcf6bed125
yellowdolphin <marius.wanko@gmail.com>
2022-01-29 15:38:49 +0100

    print centernet model stride in detect.py

--- models/common.py
+++ models/common.py
@@ -311,6 +311,10 @@ class DetectMultiBackend(nn.Module):
                 print("loading model without fuse")
                 model = torch.load(w, map_location=device)['model'].float()  # load to FP32
                 model.to(device).eval()
+                if hasattr(model, 'stride'):
+                    print(f"model.stride: {model.stride}")
+                else:
+                    print(f"WARNING: loaded model has no stride, using stride=1")
             stride = max(int(model.stride.max()), 32) if hasattr(model, 'stride') else 1  # model stride
             names = model.module.names if hasattr(model, 'module') else model.names  # get class names
             self.model = model  # explicitly assign for to(), cpu(), cuda(), half()
